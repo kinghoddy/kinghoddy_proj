38553989fa5f3257b53cfda5da19808aaa56d8fd
kinghoddy <odunmilade@gmail.com>
2022-03-21 20:45:41 +0100

    resturcture

---
+++ web_frontend/layouts/MainLayout.js
@@ -0,0 +1,133 @@
+import React from "react";
+import {
+  AppBar,
+  Container,
+  Toolbar,
+  Typography,
+  Box,
+  Menu,
+  IconButton,
+  List,
+  Button,
+  MenuItem,
+} from "@mui/material";
+
+import { makeStyles } from "@mui/styles";
+
+import MenuIcon from "@mui/icons-material/Menu";
+
+const useStyles = makeStyles({
+  root: {
+    background: "#777",
+  },
+});
+
+function MainLayout({ children }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="static">
+        <Container maxWidth="xl">
+          <Toolbar disableGutters>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ mr: 2, display: { xs: "none", md: "flex" } }}
+              style={{ color: "#f90" }}
+            >
+              KING HODDY
+            </Typography>
+
+            <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                color="inherit"
+              >
+                <MenuIcon />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorOrigin={{
+                  vertical: "bottom",
+                  horizontal: "left",
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: "top",
+                  horizontal: "left",
+                }}
+                sx={{
+                  display: { xs: "block", md: "none" },
+                }}
+              >
+                <MenuItem style={{ width: "300px" }}>
+                  <List>
+                    <Typography
+                      style={{
+                        marginRight: "40px",
+                        fontSize: "13px",
+                        color: "#f90",
+                      }}
+                    >
+                      Home
+                    </Typography>
+                  </List>
+                  <List>
+                    <Typography
+                      style={{ marginRight: "40px", fontSize: "13px" }}
+                    >
+                      My Project
+                    </Typography>
+                  </List>
+                  <Typography style={{ marginRight: "70px", fontSize: "13px" }}>
+                    Skills
+                  </Typography>
+                  <Button>Contact Me</Button>
+                </MenuItem>
+              </Menu>
+            </Box>
+            <Typography
+              variant="h6"
+              noWrap
+              component="div"
+              sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}
+              style={{ color: "#f90" }}
+            >
+              KING HODDY
+            </Typography>
+            <Box
+              style={{
+                right: "0",
+                position: "absolute",
+                marginTop: "15px",
+                marginRight: "90px",
+              }}
+              sx={{ flexGrow: 0, display: { xs: "none", md: "flex" } }}
+            >
+              <Typography
+                style={{ marginRight: "40px", fontSize: "13px", color: "#f90" }}
+              >
+                Home
+              </Typography>
+              <Typography style={{ marginRight: "40px", fontSize: "13px" }}>
+                My Project
+              </Typography>
+              <Typography style={{ marginRight: "70px", fontSize: "13px" }}>
+                Skills
+              </Typography>
+              <Button>Contact Me</Button>
+            </Box>
+          </Toolbar>
+        </Container>
+      </AppBar>
+
+      {children}
+    </div>
+  );
+}
+
+export default MainLayout;
--- web_frontend/pages/index.js
+++ web_frontend/pages/index.js
@@ -1,41 +1,39 @@
-import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
-import Container from '@mui/material/Container';
-import List from '@mui/material/List';
-import Button from '@mui/material/Button';
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import Container from "@mui/material/Container";
+import List from "@mui/material/List";
+import Button from "@mui/material/Button";
 
-import MenuItem from '@mui/material/MenuItem';
-
+import MenuItem from "@mui/material/MenuItem";
+import MainLayout from "../layouts/MainLayout";
 
 export default function Home() {
-  const mystyle =({
-
-    Appbar:{
-      background:'transparent',boxShadow:'none',color:'black',
-
+  const mystyle = {
+    Appbar: {
+      background: "transparent",
+      boxShadow: "none",
+      color: "black",
     },
-    button:{
-      color:'white',
-      width:'120px',
-      background:'blue',
-      borderRadius:'14px',
-      border:'none',
-      height:'30px'
-      ,fontSize:'10px',
-      fontWeight:'700',
-      marginTop:'-2%',
-      
+    button: {
+      color: "white",
+      width: "120px",
+      background: "blue",
+      borderRadius: "14px",
+      border: "none",
+      height: "30px",
+      fontSize: "10px",
+      fontWeight: "700",
+      marginTop: "-2%",
+    },
+  };
 
-    }
-  })
- 
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
   const handleOpenNavMenu = (event) => {
@@ -54,105 +52,8 @@ export default function Home() {
   };
 
   return (
-    <div>
-    <AppBar position="static" style={mystyle.Appbar} >
-      <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ mr: 2, display: { xs: 'none', md: 'flex' } }}
-            style={{color:'#f90'}}
-          >
-           KING HODDY
-          </Typography>
-
-          <Box  sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
-            <IconButton
-              size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
-              color="inherit"
-            >
-              <MenuIcon />
-            </IconButton>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'left',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'left',
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
-              
-              sx={{
-                display: { xs: 'block', md: 'none' },
-              }}
-            >
-             
-                <MenuItem style={{width:'300px'}} onClick={handleCloseNavMenu}   >
-                  <List>
-                <Typography style={{marginRight:'40px',fontSize:'13px',color:'#f90'}} >
-             Home
-           </Typography>
-           </List>
-           <List>
-           <Typography style={{marginRight:'40px',fontSize:'13px',}} >
-              My Project
-           </Typography>
-           </List>
-           <Typography style={{marginRight:'70px',fontSize:'13px',}} >
-             Skills
-           </Typography>
-           <Button  style={mystyle.button} >
-             Contact Me
-           </Button>
-                </MenuItem>
-           
-            </Menu>
-          </Box>
-          <Typography
-            variant="h6"
-            noWrap
-            component="div"
-            sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}
-            style={{color:'#f90'}}
-          >
-              KING HODDY
-          </Typography>
-          <Box style={{right:'0',position:'absolute',marginTop:'15px',marginRight:'90px'}} sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
-           <Typography style={{marginRight:'40px',fontSize:'13px',color:'#f90'}} >
-             Home
-           </Typography>
-           <Typography style={{marginRight:'40px',fontSize:'13px',}} >
-              My Project
-           </Typography>
-           <Typography style={{marginRight:'70px',fontSize:'13px',}} >
-             Skills
-           </Typography>
-           <Button  style={mystyle.button} >
-             Contact Me
-           </Button>
-           
-          </Box>
-
-        </Toolbar>
-      </Container>
-    </AppBar>
-    <Container  >
-      <Typography>
-uururururu
-      </Typography>
-    </Container>
-    </div>
-  )
+    <MainLayout>
+      <h1>Hello world</h1>
+    </MainLayout>
+  );
 }
