a8aa130a51a3f4162de1b2553e8cbebec81236b4
kimjay1h12 <olawaleadeit@gmail.com>
2022-03-26 13:39:35 -0700

    update

--- web_frontend/pages/index.js
+++ web_frontend/pages/index.js
@@ -1,5 +1,7 @@
 import * as React from "react";
+import RemoveIcon from '@mui/icons-material/Remove';
 import AppBar from "@mui/material/AppBar";
+import DesktopAccessDisabledIcon from '@mui/icons-material/DesktopAccessDisabled';
 import Card from "@mui/material/Card";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -18,6 +20,8 @@ import CssBaseline from "@mui/material/CssBaseline";
 import { Grid } from "@mui/material";
 
 const useStyles = makeStyles({
+  
+
   footer: {
     "& .container-footer": {
       marginLeft: "0",
@@ -27,7 +31,45 @@ const useStyles = makeStyles({
       marginTop: "2px",
     },
   },
-  header: {
+  section1:{
+   marginTop:'50px',
+textAlign:'center',
+
+marginLeft:'50px',
+marginRight:'50px',
+'& .css-i4bv87-MuiSvgIcon-root':{
+  color:'#f90',
+  fontSize:'15px',
+ fontWeight:'900'
+  
+},
+'& h1':{
+  fontSize:'15px',
+  fontWeight:'300',
+  
+  
+},
+'& h2':{
+  fontSize:'50px',
+
+  marginTop:'-1%'
+
+},
+  },
+sectioncontainer:{
+  textAlign:'center',
+  display:'flex',
+  flexDirection:'column',
+
+["@media (min-width : 1200px)"]: {
+  flexDirection:'row',
+  textAlign:'center',
+
+  justifyContent:'space-around',
+},
+
+},
+ header: {
     minHeight: "100vh",
     alignItems: "center",
     display: "flex",
@@ -55,6 +97,7 @@ const useStyles = makeStyles({
       objectFit: "contain",
     },
   },
+
 });
 
 export default function Home() {
@@ -87,7 +130,29 @@ export default function Home() {
         <Container disableGutters className="container-footer">
           <img src="/img/icons_art.png" />
         </Container>
+       
+     
       </footer>
+      <section className={classes.section1} >
+        <h1>
+         <RemoveIcon/>   TONS OF AWESOME SERVICES <RemoveIcon/>
+        </h1>
+        <h2>Our Services</h2>
+      <div className={classes.sectioncontainer} >
+        <Grid item lg={4}><img src="/img/icon1.png"/>
+        <h3>Mobile Development</h3>
+        <p>The best astonishing mobile apps with<br/> high quality users deserv.</p>
+        
+        </Grid>
+        <Grid item lg={4}><img src="/img/icon2.png"/>
+        <h3>Websites Development</h3>
+        <p>Innovative & creative website to spread<br/> your message and to express yourself.</p>
+        </Grid>
+        <Grid item lg={4}><img src="/img/icon3.png"/>
+        <h3>UI/UX Design</h3>
+        <p>We provide the best UI/UX Design by<br/>  followingthe latest trends of the market.</p></Grid>
+        </div>
+      </section>
     </MainLayout>
   );
 }
